2662d46c09bf70428f0bc968196527abac915ba0
ryanguyen206 <ryanninja595@gmail.com>
2023-04-10 13:23:45 -0700

    Add project files.

---
+++ DiscordWebApp/Data/DiscordContext.cs
@@ -0,0 +1,16 @@
+﻿using DiscordWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiscordWebApp.Data
+{
+    public class DiscordContext : DbContext
+    {
+
+        public DiscordContext(DbContextOptions<DiscordContext> options) :base(options)
+        
+        {
+
+        }
+        public DbSet<Person> Persons { get; set; }
+    }
+}
